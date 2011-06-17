26aabcdb3af11be58f6275d74f99a52d6e3d2064
vnphoneapps <vnphoneapps@gmail.com>
2011-06-17 14:21:11 +0700

    add new file

---
+++ TestGitProject/src/git/practice/test/TestGitProject.java
@@ -0,0 +1,13 @@
+package git.practice.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestGitProject extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
